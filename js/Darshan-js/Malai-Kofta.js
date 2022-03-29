f758b3f72811f480e19141c03aba8ee8f8a039e6
Darshan Patel <97763139+DarshankumarPatel02@users.noreply.github.com>
2022-03-29 16:54:04 -0400

    Update Malai-Kofta.js

--- js/Darshan-js/Malai-Kofta.js
+++ js/Darshan-js/Malai-Kofta.js
@@ -1,15 +1,17 @@
+// List of images store in array
 let myImagesArray = ["Darshan-Images/FinalKofta.jpg","Darshan-Images/Kofta1.jpg","Darshan-Images/Kofta2.jpg","Darshan-Images/Kofta3.jpg",
                     "Darshan-Images/Kofta4.jpg","Darshan-Images/Paste1.jpg","Darshan-Images/Paste2.jpg","Darshan-Images/Paste3.jpg"];
 
+// Responsive Navigation Hamburgur Menu with show and hide function
 var navList = document.getElementById("nav-lists");
 function Show() {
-  navList.classList.add("_Menus-show");
+  navList.classList.add("_Menus-show");  //Show Menu on click
 }
 function Hide() {
-  navList.classList.remove("_Menus-show");
+  navList.classList.remove("_Menus-show");  //Hide Menu on click
 }
 
-var i = myImagesArray.length;
+var i = myImagesArray.length; //calculate array length
 // function for next slide 
 function nextImage(){
   if (i<myImagesArray.length) {
@@ -17,7 +19,7 @@ function nextImage(){
   }else{
       i = 1;
   }
-  document.getElementById('ControlImage').src=myImagesArray[i-1];
+  document.getElementById('ControlImage').src=myImagesArray[i-1]; //move to next image on clicking arrow(>)
 }
 // function for prew slide
 function prewImage(){
@@ -26,7 +28,7 @@ function prewImage(){
   }else{
       i = myImagesArray.length;
   }
-  document.getElementById('ControlImage').src=myImagesArray[i-1];
+  document.getElementById('ControlImage').src=myImagesArray[i-1];  //move to next image on clicking arrow(<)
 }
 // script for auto image slider
 //setInterval(nextImage , 4000);
