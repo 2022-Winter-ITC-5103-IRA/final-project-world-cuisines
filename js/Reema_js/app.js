1c0c7af211f506abf47d7206d98110c5118c19cb
Reema1512 <97650704+Reema1512@users.noreply.github.com>
2022-04-04 22:26:27 -0400

    Add files via upload

--- js/Reema_js/app.js
+++ js/Reema_js/app.js
@@ -29,3 +29,36 @@ function Submit() {
 function reset() {    
   document.getElementById("comment").value = "";
 }
+
+var coll = document.getElementsByClassName("collapse");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
+const btn = document.querySelector("button");
+      const post = document.querySelector(".post");
+      const widget = document.querySelector(".star-widget");
+      const editBtn = document.querySelector(".edit");
+  
+      btn.onclick = ()=>{
+        widget.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget.style.display = "block";
+          post.style.display = "none";
+          
+        }
+        return false;
+      }
