bd9fabee9f0acf29967ca2d67f3ab38980b0f159
Reema1512 <97650704+Reema1512@users.noreply.github.com>
2022-04-06 09:41:04 -0400

    Add files via upload

--- js/Reema_js/app.js
+++ js/Reema_js/app.js
@@ -9,19 +9,58 @@ function Hide() {
   navList.classList.remove("_Menus-show");
 }
 
-// this function helps in asking the user to input their email if they click on the check box
-function myFunction() {
-  var checkBox = document.getElementById("myCheck");
-  // if checkbox is checked then one dialog box will be open and ask the user to enter their email id
-  if (checkBox.checked == true) {
-    prompt("Enter the email");
+function validateEmail(x, email)
+{
+  var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if(email.value.match(mailformat))
+  {
+    window.alert("Thank you for submitting");
+    myFunction(x);
+  document.form1.text1.focus();
+  return true;
   }
+  else
+  {
+  alert("Invalid email address.");
+  document.form1.text1.focus();
+  return false;
+  }
+ 
+} 
+
+// this function helps in asking the user to input their email if they click on the check box
+function myFunction(x) {
+  
+  email.remove();
+  x.style.display = 'none';
+  
+
 }
 
 // this function is used to clear the text area when submit button will be clicked
 function Submit() {
   window.alert("Thank you for submitting");
   reset();
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
 //This is the clearing function
