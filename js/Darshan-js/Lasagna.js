d43078c50c4657eb8f57e508f83368075ef67baf
Darshan Patel <97763139+DarshankumarPatel02@users.noreply.github.com>
2022-04-07 16:10:20 -0400

    Add files via upload

---
+++ js/Darshan-js/Lasagna.js
@@ -0,0 +1,8 @@
+// Responsive Navigation Hamburgur Menu with show and hide function
+var navList = document.getElementById("nav-lists");
+function Show() {
+  navList.classList.add("_Menus-show");  //Show Menu on click
+}
+function Hide() {
+  navList.classList.remove("_Menus-show");  //Hide Menu on click
+}
