e35eafa08bbc4b4fb00bf2bc579247af9461815f
KAIYAN-CHEN <98044243+KAIYAN-CHEN@users.noreply.github.com>
2022-04-07 15:52:04 -0400

    Add files via upload

--- js/js_Kaiyan/feature1_slide_automatic_Kaiyan.js
+++ js/js_Kaiyan/feature1_slide_automatic_Kaiyan.js
@@ -1,4 +1,5 @@
-let myImagesArray = ["images_Kaiyan/breakfast-burrito.jpg","images_Kaiyan/breakfast-burrito2.jpg","images_Kaiyan/breakfast-burrito3.jpg"];
+// automatic slide
+let myImagesArray = ["images/images_Kaiyan/breakfast-burrito.jpg","images/images_Kaiyan/breakfast-burrito2.jpg","images/images_Kaiyan/breakfast-burrito3.jpg"];
 
 let ImageNumber = 0;
 let difference = myImagesArray.length -1;
@@ -21,9 +22,33 @@ function ChangeImages(direction)
 		{
 			ImageNumber = difference;
 		}
-		//document.slideshow.src = myImagesArray[ImageNumber];
+		
 		document.getElementById('slideshow').src = myImagesArray[ImageNumber];
 
        
 
     }
+
+
+// the event handler for the click event
+const toggle = (evt) => {
+	console.log(evt);
+	const h2Element = evt.currentTarget; 
+	const divElement = h2Element.nextElementSibling; 
+
+	h2Element.classList.toggle('minus');
+	divElement.classList.toggle('open');
+
+	evt.preventDefault(); 
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const h2Elements = step.querySelectorAll('#step h2');
+	
+	for (let h2Element of h2Elements) {
+	h2Element.addEventListener('click', toggle);
+	}
+	
+	h2Elements[0].firstChild.focus();
+});
