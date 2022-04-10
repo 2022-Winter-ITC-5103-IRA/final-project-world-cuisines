c321ec87010c6a78de1fab75166ebe7792fff7f9
Darshan Patel <97763139+DarshankumarPatel02@users.noreply.github.com>
2022-04-10 18:04:21 -0400

    Add files via upload

--- js/Darshan-js/Lasagna.js
+++ js/Darshan-js/Lasagna.js
@@ -7,25 +7,28 @@ function Hide() {
   navList.classList.remove("_Menus-show");  //Hide Menu on click
 }
 
-document.getElementById('CalorieMeter').addEventListener('submit', function(e){
-  document.getElementById('results').style.display = 'none';
-  document.getElementById('loading').style.display = 'block';
+document.getElementById('CalorieMeter').addEventListener('submit', function(e){ //form action when submit is click
+  document.getElementById('results').style.display = 'none';  
+  document.getElementById('loading').style.display = 'block'; 
   setTimeout(calculateCalories, 2000);
   e.preventDefault();
 });
 
 function calculateCalories(e) {
   
-  const age = document.getElementById('age');
-  const gender = document.querySelector('input[name="Gender"]:checked');
+  //access all fields there in calrie calculator using variables
+  const age = document.getElementById('age');  
+  const gender = document.querySelector('input[name="Gender"]:checked'); /*By default Male*/
   const height = document.getElementById('height');
   const weight = document.getElementById('weight');
-  const activity = document.getElementById('list').value;
+  const activity = document.getElementById('list').value; //it access value of activity(by default 1)
   const totalCalories = document.getElementById('total-calories');
   
   if (age.value === '' || weight.value === '' || height.value === '' || 80 < age.value || age.value < 15) {
-    errorMessage('Please make sure the values you entered are correct')
-  }else if(gender.id === 'Male' && activity === "1") {
+    errorMessage('Please make sure the values you entered are correct') //validation not done 
+  }
+  /*Main based on Gender Selection and acitivity selection value change */
+  else if(gender.id === 'Male' && activity === "1") {
     totalCalories.value = 1.2 * (66.5 + (13.75 * parseFloat(weight.value)) + 
                           (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
   }else if(gender.id === 'Male' && activity === "2") {
@@ -56,22 +59,22 @@ function calculateCalories(e) {
     totalCalories.value = 1.9 * (655 + (9.563 * parseFloat(weight.value)) + 
                           (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
   } 
-  document.getElementById('results').style.display = 'block';
-  document.getElementById('loading').style.display = 'none'; 
+  document.getElementById('results').style.display = 'block'; //display result
+  document.getElementById('loading').style.display = 'none';  //hiding loading 
 }
 
 function errorMessage(error) {
-  document.getElementById('results').style.display = 'none';
+  //error then does not display loading and result
+  document.getElementById('results').style.display = 'none'; 
   document.getElementById('loading').style.display = 'none';
-  const errorDiv = document.createElement('div');
+  const errorDiv = document.createElement('div'); //create div element
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
-  errorDiv.className = 'alert alert-danger';
+  errorDiv.className = 'alert alert-danger'; 
   errorDiv.appendChild(document.createTextNode(error));
   card.insertBefore(errorDiv, heading);
-
-  setTimeout(clearError, 4000);
+  setTimeout(clearError, 5000); //display error message in div for 5 seconds
 }
 function clearError() {
-  document.querySelector('.alert').remove();
+  document.querySelector('.alert').remove(); //remove container for error
 }
