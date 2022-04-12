b924721f7642ffccce99facdee4f3dbe0aef7886
heli-28 <97644383+heli-28@users.noreply.github.com>
2022-04-11 23:26:57 -0400

    Add files via upload

--- js/helina-js/helina2.js
+++ js/helina-js/helina2.js
@@ -1,78 +1,120 @@
 // this function will be called by clicking side button and will be redirected to the particluar division mentioned by giving id
-function scrollEvent(division){
+function scrollEvent(division) {
   const element = document.getElementById(division);
-  element.scrollIntoView({behavior: 'smooth', block:'center'});
+  element.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
 // onclicking the hamburger menu will be shown when a responsive screen is used
-function showMenu(){
-  var content= document.getElementById('LeftSlider');
-  if(window.getComputedStyle(content).display === 'none'){
-      content.style.display = 'block';   
+function showMenu() {
+  var content = document.getElementById('LeftSlider');
+  if (window.getComputedStyle(content).display === 'none') {
+    content.style.display = 'block';
   }
 }
 
 // onclicking the close button menu will be hidden when a responsive screen is used
-function hideMenu(){
-  var content= document.getElementById('LeftSlider');
-  if(window.getComputedStyle(content).display === 'block'){
-      content.style.display = 'none';   
+function hideMenu() {
+  var content = document.getElementById('LeftSlider');
+  if (window.getComputedStyle(content).display === 'block') {
+    content.style.display = 'none';
   }
 }
 
 // image slider
-var counter=1;
-setInterval(function(){
-  document.getElementById('radio' + counter).checked=true;
+var counter = 1;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
   counter++;
-  if(counter>4){
-    counter=1;
+  if (counter > 4) {
+    counter = 1;
   }
 }, 2000);
 
 // automatic popup
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   setTimeout(
-      function open(event){
-          document.querySelector(".popup").style.display = "block";
-      },
-      2000 
+    function open(event) {
+      document.querySelector(".popup").style.display = "block";
+    },
+    2000
   )
 });
-document.querySelector("#close").addEventListener("click", function(){
+document.querySelector("#close").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "none";
 });
 
 // form validation
-function validate(){
+function validate() {
   var name = document.getElementById("name").value;
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
   var error_message = document.getElementById("error_message");
-  
+
   error_message.style.padding = "10px";
-  
+
   var text;
-  if(name.length < 5){
-    text = "Please Enter valid Name";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(isNaN(phone) || phone.length != 10){
+  // if(name.length < 5){
+  //   text = "Please Enter valid Name";
+  //   error_message.innerHTML = text;
+  //   return false;
+  // }
+  if (isNaN(phone) || phone.length != 10) {
     text = "Please Enter valid Phone Number";
     error_message.innerHTML = text;
-    return false;
+    // return false;
   }
-  if(email.indexOf("@") == -1 || email.length < 6){
+  if (email.indexOf("@") == -1) {
     text = "Please Enter valid Email";
     error_message.innerHTML = text;
-    return false;
+    // return false;
   }
-  if(message.length <= 140){
-    text = "Please Enter More Than 140 Characters";
-    error_message.innerHTML = text;
-    return false;
-  }
+  // if(message.length <= 140){
+  //   text = "Please Enter More Than 140 Characters";
+  //   error_message.innerHTML = text;
+  //   return false;
+  // }
   alert("Form Submitted Successfully!");
-  return true;
+  // return true;
+
 }
+
+$(document).ready(function () {
+
+  $('#submit').change(function () {
+    alert("Hello");
+    // var er = $('#ErNo').val();
+    // $('#weeklyEr,#MidEr').val(er);
+    // $.ajax({
+    //   headers: { "X-CSRFToken": token },
+    //   type: "POST",
+    //   url: "getData",
+    //   dataType: "JSON",
+    //   data: { Enrollment: er },
+    //   success: function (Received) {
+    //     if (Received.weekly.Status == 'ok') {
+    //       $('#weeklyAP').val(Received.weekly.Wap)
+    //       $('#weeklyPDC').val(Received.weekly.Wpdc)
+    //       $('#weeklySE').val(Received.weekly.Wse)
+    //       $('#weeklyWDD').val(Received.weekly.Wwdd)
+    //     } else {
+    //       $('#weeklyAP').val('')
+    //       $('#weeklyPDC').val('')
+    //       $('#weeklySE').val('')
+    //       $('#weeklyWDD').val('')
+    //     }
+
+    //     if (Received.mid.Status == 'ok') {
+    //       $('#midAP').val(Received.mid.Map)
+    //       $('#midPDC').val(Received.mid.Mpdc)
+    //       $('#midSE').val(Received.mid.Mse)
+    //       $('#midWDD').val(Received.mid.Mwdd)
+    //     } else {
+    //       $('#midAP').val('')
+    //       $('#midPDC').val('')
+    //       $('#midSE').val('')
+    //       $('#midWDD').val('')
+    //     }
+    //   }
+    // });
+  });
+});
