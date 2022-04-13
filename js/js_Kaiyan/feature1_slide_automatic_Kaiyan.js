6bb13cf5918c847c97cf1a5bb202a2bdd96bde94
KAIYAN-CHEN <cky8023wlh@163.com>
2022-04-13 11:54:30 -0400

    updated

--- js/js_Kaiyan/feature1_slide_automatic_Kaiyan.js
+++ js/js_Kaiyan/feature1_slide_automatic_Kaiyan.js
@@ -30,30 +30,7 @@ function ChangeImages(direction)
     }
 
 
-// the event handler for the click event
-// const toggle = (evt) => {
-// 	console.log(evt);
-// 	const h2Element = evt.currentTarget; 
-// 	const divElement = h2Element.nextElementSibling; 
 
-// 	h2Element.classList.toggle('minus');
-// 	divElement.classList.toggle('open');
-
-// 	evt.preventDefault(); 
-// };
-
-// document.addEventListener('DOMContentLoaded', () => {
-	
-// 	const h2Elements = step.querySelectorAll('#step h2');
-	
-// 	for (let h2Element of h2Elements) {
-// 	h2Element.addEventListener('click', toggle);
-// 	}
-	
-// 	h2Elements[0].firstChild.focus();
-// });
-
-//dynamic content
 let comments = ["This looks so good, can't wait to try it!", "It only takes me 15 minutes, easy for breakfast!", "Super loooooove!!!"];
 
 display = () => {
